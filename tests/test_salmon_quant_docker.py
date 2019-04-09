5ac627846b228a2ab8ac8a5df69fc5ed40d77953
tonyyzy <38984697+tonyyzy@users.noreply.github.com>
2019-04-09 16:22:08 +0100

    Update tests

--- tests/test_salmon_quant_docker.py
+++ tests/test_salmon_quant_docker.py
@@ -1,5 +1,12 @@
 import subprocess
 import filecmp
+import csv
+
+
+def salmon_quant_reader(filepath):
+	result = float(list(csv.reader(open(filepath), delimiter="\t"))[1][3])
+	return round(result)
+
 
 def test_salmon_quant_single():
 	subprocess.run(["cwl-runner",
@@ -9,13 +16,15 @@ def test_salmon_quant_single():
 	assert filecmp.cmp("./tests/salmon_quant/test3/quant.sf",
 						"./test_salmon_quant_single/test3/quant.sf")
 
+
 def test_salmon_quant_pe():
 	subprocess.run(["cwl-runner",
 					"--outdir=./test_salmon_quant_pe",
 					"./tools/salmon_quant.cwl",
 					"./tools/salmon_quant.yml"])
-	assert filecmp.cmp("./tests/salmon_quant/test2/quant.sf",
-						"./test_salmon_quant_pe/test2/quant.sf")
+	ref = salmon_quant_reader("./tests/salmon_quant/test2/quant.sf")
+	test = salmon_quant_reader("./test_salmon_quant_pe/test2/quant.sf")
+	assert ref == test
 
 
 if __name__ == "__main__":
